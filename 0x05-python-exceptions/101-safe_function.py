b2f0c12f75526214232e378977b61a6d148b6b1b
TobiMoscow <tobimoscow@gmail.com>
2022-12-19 13:39:32 +0100

    Code

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-
-import sys
-
-
-
+from sys import stderr
 
 
 def safe_function(fct, *args):
-
     try:
-
         return fct(*args)
-
-    except Exception as e:
-
-        print("Exception: {}".format(e), file=sys.stderr)
-
+    except (ZeroDivisionError, IndexError, TypeError, ValueError) as e:
+        stderr.write("Exception: {}\n".format(e))
         return None
